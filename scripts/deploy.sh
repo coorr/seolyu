fe9d6bf5858edbb56cdd0bbf68eba5ed4a6197f3
gimjinseong <wlsdiqkdrk@naver.com>
2024-10-10 13:28:20 +0900

    docs: deploy.sh, appspec.yml 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,8 +30,8 @@ if [ -z $CURRENT_PID1 ]
 then
   log "> 첫 번째 애플리케이션이 구동 중이 아니므로 종료하지 않습니다."
 else
-  log "> 첫 번째 애플리케이션 종료 : kill -15 $CURRENT_PID1"
-  kill -15 $CURRENT_PID1
+  log "> 첫 번째 애플리케이션 종료 : kill -9 $CURRENT_PID1"
+  sudo kill -9 $CURRENT_PID1
   log "> Waiting for 5 seconds..."
   sleep 5
 fi
@@ -39,6 +39,9 @@ fi
 # 2. 첫 번째 인스턴스에 새 버전 배포
 log "> 첫 번째 인스턴스에 새 버전 배포 (PORT: $PORT1)"
 nohup java -jar \
+  -javaagent:/home/ubuntu/pinpoint/pinpoint-agent-3.0.0/pinpoint-bootstrap-3.0.0.jar \
+  -Dpinpoint.agentId=prd-seolyu \
+  -Dpinpoint.applicationName=prd-seolyuBack \
   -Dspring.config.location=$REPOSITORY/application-config.yaml  \
   -Dspring.config.location=$REPOSITORY/application.yaml  \
   -Dspring.profiles.active=prd  \
@@ -69,53 +72,54 @@ do
     fi
 done
 
-# 4. 두 번째 인스턴스 종료 및 첫 번째 인스턴스로 트래픽 이동
-log "> 현재 구동중인 두 번째 애플리케이션(pid 확인)"
-CURRENT_PID2=$(pgrep -f ".*-Dserver.port=$PORT2.*.jar")
-
-log "> 현재 구동중인 두 번째 애플리케이션 pid : $CURRENT_PID2"
-
-if [ -z $CURRENT_PID2 ]
-then
-  log "> 두 번째 애플리케이션이 구동 중이 아니므로 종료하지 않습니다."
-else
-  log "> 두 번째 애플리케이션 종료 : kill -15 $CURRENT_PID2"
-  kill -15 $CURRENT_PID2
-  log "> Waiting for 5 seconds..."
-  sleep 5
-fi
-
-# 5. 두 번째 인스턴스에 새 버전 배포
-log "> 두 번째 인스턴스에 새 버전 배포 (PORT: $PORT2)"
-nohup java -jar \
-  -Dspring.config.location=$REPOSITORY/application-config.yaml  \
-  -Dspring.config.location=$REPOSITORY/application.yaml  \
-  -Dspring.profiles.active=prd  \
-  -Dserver.port=$PORT2 \
-  $JAR_NAME > $REPOSITORY/nohup2.out 2>&1 &
-
-# 6. 두 번째 인스턴스 헬스 체크
-log "> 두 번째 인스턴스 헬스 체크 시작"
-
-for i in $(seq 1 10)
-do
-    RESPONSE=$(curl -s http://localhost:$PORT2/health)
-    OK_COUNT=$(echo "$RESPONSE" | grep -c 'ok')
-
-    if [ $OK_COUNT -ge 1 ]
-    then
-        log "> 두 번째 인스턴스가 성공적으로 시작되었습니다."
-        break
-    else
-        log "> 두 번째 인스턴스가 아직 시작되지 않았습니다. 10초 후 다시 시도합니다."
-        sleep 10
-    fi
-
-    if [ $i -eq 10 ]
-    then
-        log "> 두 번째 인스턴스가 시작되지 않았습니다. 배포를 중단합니다."
-        exit 1
-    fi
-done
+## 4. 두 번째 인스턴스 종료 및 첫 번째 인스턴스로 트래픽 이동
+#log "> 현재 구동중인 두 번째 애플리케이션(pid 확인)"
+#CURRENT_PID2=$(pgrep -f ".*-Dserver.port=$PORT2.*.jar")
+#
+#log "> 현재 구동중인 두 번째 애플리케이션 pid : $CURRENT_PID2"
+#
+#if [ -z $CURRENT_PID2 ]
+#then
+#  log "> 두 번째 애플리케이션이 구동 중이 아니므로 종료하지 않습니다."
+#else
+#  log "> 두 번째 애플리케이션 종료 : kill -15 $CURRENT_PID2"
+#  kill -15 $CURRENT_PID2
+#  log "> Waiting for 5 seconds..."
+#  sleep 5
+#fi
+#
+## 5. 두 번째 인스턴스에 새 버전 배포
+#log "> 두 번째 인스턴스에 새 버전 배포 (PORT: $PORT2)"
+#nohup java -jar \
+#  -Dspring.config.location=$REPOSITORY/application-config.yaml  \
+#  -Dspring.config.location=$REPOSITORY/application.yaml  \
+#  -Dspring.profiles.active=prd  \
+#  -Dserver.port=$PORT2 \
+#  $JAR_NAME > $REPOSITORY/nohup2.out 2>&1 &
+#
+## 6. 두 번째 인스턴스 헬스 체크
+#log "> 두 번째 인스턴스 헬스 체크 시작"
+#
+#for i in $(seq 1 10)
+#do
+#    RESPONSE=$(curl -s http://localhost:$PORT2/health)
+#    OK_COUNT=$(echo "$RESPONSE" | grep -c 'ok')
+#
+#    if [ $OK_COUNT -ge 1 ]
+#    then
+#        log "> 두 번째 인스턴스가 성공적으로 시작되었습니다."
+#        break
+#    else
+#        log "> 두 번째 인스턴스가 아직 시작되지 않았습니다. 10초 후 다시 시도합니다."
+#        sleep 10
+#    fi
+#
+#    if [ $i -eq 10 ]
+#    then
+#        log "> 두 번째 인스턴스가 시작되지 않았습니다. 배포를 중단합니다."
+#        exit 1
+#    fi
+#done
 
 log ">------------------------배포 종료------------------------"
+
