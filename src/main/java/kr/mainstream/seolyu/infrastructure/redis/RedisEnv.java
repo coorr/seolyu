c5a41a4d609530958a0c672127ff653f2334065c
gimjinseong <wlsdiqkdrk@naver.com>
2024-10-23 14:29:24 +0900

    temp : 임시 코드 추가

--- src/main/java/kr/mainstream/seolyu/infrastructure/redis/RedisEnv.java
+++ src/main/java/kr/mainstream/seolyu/infrastructure/redis/RedisEnv.java
@@ -6,6 +6,6 @@ public class RedisEnv {
     public static final int LOGIN_INFO_TTL = 30;
     public static final String LOGIN_INFO_KEY = "loginInfo";
     public static final String EVENT_KEY = "event";
-    public static final int EVENT_KEY_TTL = 1;
+    public static final int EVENT_KEY_TTL = 5;
     public static final String EVENT_REQUEST_WITH_EVENT_ID_PREFIX = "event.request.eventId=%s";
 }
